3e994584fed77a30c4bce13dded3d0841cba5fac
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-06-21 17:51:56 +0100

    refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,56 +4,101 @@ class GildedRose
     @items = []
     items.each { |item| @items.push(item) }
   end
-
+  
   def update_quality()
     @items.each do |item|
-      return item if item.name == 'Sulfuras'
-      if item.name != "Aged Brie" && item.name != "Backstage passes"
-        if item.quality > 0 
-          if item.name != "Sulfuras"
-            item.quality = item.quality - 1
-          end
-        end
+      next if item.name == 'Sulfuras'
+  
+      if item.name == "Aged Brie"
+        increase_quality(item)
+      elsif item.name == "Backstage passes"
+        increase_quality(item)
+        increase_quality(item) if item.sell_in < 11
+        increase_quality(item) if item.sell_in < 6
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-
-        end
+        decrease_quality(item)
       end
-      if item.name != "Sulfuras"
-        item.sell_in = item.sell_in - 1
-      end
+  
+      decrease_sell_in(item)
+  
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes"
-            if item.quality > 0
-              if item.name != "Sulfuras"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+        if item.name == "Aged Brie"
+          increase_quality(item)
+        elsif item.name == "Backstage passes"
+          item.quality -= item.quality
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          decrease_quality(item)
         end
       end
     end
   end
+  
+  def increase_quality(item)
+    return if item.quality >= 50
+  
+    item.quality += 1
+  end
+  
+  def decrease_quality(item)
+    return if item.quality <= 0
+  
+    item.quality -= 1
+  end
+  
+  def decrease_sell_in(item)
+    return if item.name == "Sulfuras"
+  
+    item.sell_in -= 1
+  end
+  
+  # def update_quality()
+  #   @items.each do |item|
+  #     return item if item.name == 'Sulfuras'
+  #     if item.name != "Aged Brie" && item.name != "Backstage passes"
+  #       if item.quality > 0 
+  #         if item.name != "Sulfuras"
+  #           item.quality -= 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality += 1
+  #         if item.name == "Backstage passes"
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality += 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality += 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != "Sulfuras"
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras"
+  #               item.quality -= 1
+  #             end
+  #           end
+  #         else
+  #           item.quality -= item.quality
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality += 1
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 class Item
