1a7ae2d90417aadac2824d8da38f0ca8e5990ea4
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-06-21 13:24:51 +0100

    pass 2 tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,17 @@
 require 'gilded_rose'
 
 describe GildedRose do
-
-  describe "#update_quality" do
-    it "does not change the name" do
-      item = Item.new("foo", 0, 0)
+  describe '#update_quality' do
+    it 'does not change the name' do
+      item = Item.new('foo', 0, 0)
       GildedRose.new(item).update_quality()
-      expect(item.name).to eq "foo"
+      expect(item.name).to eq 'foo'
+    end
+    it 'decreases a standard item by 1 in sell-in and quality' do
+      glass = Item.new('opaque', 10, 10)
+      GildedRose.new(glass).update_quality
+      expect(glass.sell_in).to eq 9
+      expect(glass.quality).to eq 9
     end
   end
-
 end
