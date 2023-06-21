263f4c3ab1d5233a76140905120d40b5e460f074
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-06-21 13:38:52 +0100

    pass 6 tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -20,16 +20,23 @@ describe GildedRose do
       expect(glass.quality).to eq(0)
     end
     it 'cannot increase an item\'s quality above 50' do
-      glass = Item.new('Aged Brie', 10, 50)
-      GildedRose.new(glass).update_quality
-      expect(glass.sell_in).to eq(9)
-      expect(glass.quality).to eq(50)
+      cheese = Item.new('Aged Brie', 10, 50)
+      GildedRose.new(cheese).update_quality
+      expect(cheese.sell_in).to eq(9)
+      expect(cheese.quality).to eq(50)
     end
     it 'a standard item\'s quality decreases twice as fast once past the sell-in date' do
       glass = Item.new('opaque', 0, 10)
       GildedRose.new(glass).update_quality
-      expect(glass.sell_in).to eq(-1)
-      expect(glass.quality).to eq(8)
+      GildedRose.new(glass).update_quality
+      expect(glass.sell_in).to eq(-2)
+      expect(glass.quality).to eq(6)
+    end
+    it 'increases Aged Brie overtime' do
+      cheese = Item.new('Aged Brie', 10, 30)
+      GildedRose.new(cheese).update_quality
+      expect(cheese.sell_in).to eq(9)
+      expect(cheese.quality).to eq(31)
     end
   end
 end
